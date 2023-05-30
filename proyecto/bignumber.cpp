179849322a2ff63090f2cf007fd39860dfad3ede
jdmejias <118640031+jdmejias@users.noreply.github.com>
2023-05-29 23:40:30 -0500

    Proyecto final

--- proyecto/bignumber.cpp
+++ proyecto/bignumber.cpp
@@ -1,150 +1,399 @@
-#include "bignumber.cpp"
-
+  #include "bignumber.h"
+/*
+Funcion: Constructor de la clase BigInteger 
+Entrada:  recibe una cadena de caracteres.
+Descripcion: Este constructor lo que hace es crear una instancia de BigInteger 
+*/
 BigInteger::BigInteger(const string& cadena){
         int start=0;
         if(cadena[0] == '-') { 
             start= 1;
-            signo = false;
-        } else {
             signo = true;
+        } else {
+            signo = false;      
         }
         for (int i = start; i < cadena.size(); i++) {
             vec1.push_back(cadena[i] - '0');
         }
     }
+/*
+Funcion: Constructor de la clase BigInteger
+Entrada: Un objeto BigIngeger.
+Descripcion: Lo que hace es crear una instancia de BigInteger usando otro objeto BigInteger
+*/
 BigInteger::BigInteger(const BigInteger& bignumber) {
         for(int i=0; i < bignumber.vec1.size(); i++)
             vec1.push_back(bignumber.vec1[i]);
         signo = bignumber.signo;
     }
-    
-BigInteger operator+(BigInteger bignumber);
-BigInteger operator-(BigInteger bignumber);
-BigInteger operator*(BigInteger bignumber);
-BigInteger operator/(BigInteger bignumber);
-BigInteger operator+(BigInteger bignumber);
-BigInteger operator%(BigInteger bignumber);
-bool operator==(BigInteger bignumber);
-bool operator<(BigInteger bignumber);
-bool operator<=(BigInteger bignumber);
+/*
+Funcion: Sobrecarga del operador suma para BigInteger
+Entrada: Un objeto BigInteger
+Salida: Un objeto BigInteger
+Descripcion: Realiza la suma de los dos BigIntegeres usando add y devuelve la suma
+*/    
+BigInteger BigInteger::operator+(BigInteger& bignumber){
+    BigInteger suma(*this);
+    suma.add(bignumber);
+    return suma;
+}
+/*
+Funcion: Sobrecarga del operador resta para BigInteger
+Entrada: Un objeto BigInteger
+Salida: Un objeto BigInteger
+Descripcion:Realiza la resta de dos BigIntegeres usando substract y devuelve la resta.
+*/ 
+BigInteger BigInteger::operator-(BigInteger& bignumber){
+    BigInteger resta(*this);
+    resta.substract(bignumber);
+    return resta;
+}
+/*
+Funcion: Sobrecarga del operador multiplicar para Biginteger
+Entrada: Un objeto BigInteger
+Salida: Un objeto BigInteger
+Descripcion:  Realiza la multiplicacion de dos BigInteger usando la funcion product y un Biginteger nuevo con dicha multiplicacion.
+*/ 
+BigInteger BigInteger::operator*(BigInteger& bignumber){
+    BigInteger multiplicacion(*this);
+    multiplicacion.product(bignumber);
+    return multiplicacion;
+}
+/*
+Funcion:  Sobrecarga del operador dividir para Biginteger
+Entrada: Un objeto BigInteger
+Salida: Un objeto BigInteger
+Descripcion: Realiza la division de dos BigInteger usando la funcion quotient que es dividir, y retorna un BigInteger nuevo con dicha division.
+*/ 
+BigInteger BigInteger::operator/(BigInteger& bignumber){
+    BigInteger division(*this);
+    division.quotient(bignumber);
+    return division;
+}
+/*
+Funcion: Sobrecarga del operador modulo para BigInteger
+Entrada: Un objeto BigInteger
+Salida: Un objeto BigInteger
+Descripcion: Lo que hace es sacar el residuo o modulo de dos BigInteger creando un nuevo BigInteger con el resultado.
+*/ 
+BigInteger BigInteger::operator%(BigInteger& bignumber){
+    BigInteger modulo(*this);
+    modulo.remainder(bignumber);
+    return modulo;
+}
+/*
+Funcion: Sobrecarga del operador igual igual
+Entrada: Un objeto BigInteger
+Salida: Verdadero o Falso.
+Descripcion:Lo que hace es comparar dos BigInteger, si son iguales retorno true en lo contrario falso.
+*/ 
+bool  BigInteger::operator==(BigInteger& bignumber){
+        bool ans=true;
+        int i=0;
+        if(vec1.size()!= bignumber.vec1.size())
+            ans=false;
+        while(ans && i < vec1.size()){
+            if(vec1[i]!=bignumber.vec1[i])
+                ans=false;
+            i++;
+         }
+        if(vec1.empty() && bignumber.vec1.empty())
+            ans=true;
 
+        return ans; 
+    }
+/*
+Funcion: Sobrecarga del operador menor 
+Entrada: Un objeto BigInteger
+Salida: Verdadero o Falso
+Descripcion: Lo que hace es comparar dos BigInteger, si es menor retorna verdadero en lo contrario falso.
+*/ 
+bool BigInteger::operator<(BigInteger& bignumber) {
+        bool result = false;
+        bool encontrar = false;
+        if (signo && !bignumber.signo) {
+            encontrar = true;
+            result = true;   
+        }
+        else if (!signo && bignumber.signo) 
+            encontrar = true;
+        else if (vec1.size() > bignumber.vec1.size()) { 
+            if (signo && bignumber.signo) { 
+                result = true;
+                encontrar = true;
+            }
+            else 
+                encontrar = true;
+        }
+        else if (vec1.size() < bignumber.vec1.size()) { 
+            if (signo && bignumber.signo) 
+                encontrar = true;
+            else {
+                result = true;
+                encontrar = true;
+            }
+        }
+        
+        else if(!encontrar) {
+                int i = 0;
+                while (i < vec1.size() && vec1[i] == bignumber.vec1[i]) {
+                    i++;
+                }
+                if (i < vec1.size() && vec1[i] < bignumber.vec1[i]) {
+                    if (signo && bignumber.signo) {
+                        result = false;
+                    }
+                    else {
+                        result = true;
+                    }
+                }
+                else{
+                    if (signo && bignumber.signo) {
+                        result = true;
+                    }
+                    else {
+                        result = false;
+                    }
+                }
+        }
 
-void BigInteger::add(BigInteger& bignumber){
+        return result;
+    }
+/*
+Funcion: Sobrecarga del operador <=
+Entrada: Un objeto BigInteger
+Salida: Verdadero o Falso
+Descripcion: Lo que hace es comparar dos BigInteger si es menor o igual retorna verdadero en lo contrario falso.
+*/ 
+bool BigInteger::operator<=(BigInteger& bignumber) {
+    ans=false;
+    if((*this < bignumber)|| (*this == bignumber))
+        ans=true;
+        
+    return ans;
+}
+/*
+Funcion: add
+Entrada: Un objeto BigInteger
+Descripcion: Lo que hace es sumarle un BigInteger a la entrada la cual es otro BigInteger, al final queda el BigInteger que uso la funcion modificado con la suma de ambos BigInteger.
+*/ 
+void BigInteger::add(BigInteger& bignumber) {
     int z = 0;
     vector<int> vec2 = bignumber.vec1;
-    vector<int> vec3;
     int max1 = vec1.size();
     int max2 = vec2.size();
-    int distancia = 0;
-    if (max1 >= max2) {
-        distancia = max1 - max2;
-        for (int i = 0; i < distancia; i++)
-            vec2.insert(vec2.begin(), 0);
-    } else {
-        distancia = max2 - max1;
-        for (int i = 0; i < distancia; i++)
-            vec1.insert(vec1.begin(), 0);
-    }
-    for (int j = vec1.size() - 1; j >= 0; j--) {
-        if (vec1[j] + vec2[j] + z >= 10) {
-            vec3.insert(vec3.begin(), (vec1[j] + vec2[j] + z) % 10);
-            z = 1;
-        } else {
-            vec3.insert(vec3.begin(), vec1[j] + vec2[j] + z);
-            z = 0;
-        }
-    }
-    if (z == 1)
-        vec3.insert(vec3.begin(), 1);
+    int tam = max(max1, max2) + 1;
+    vector<int> vec3(tam, 0);
 
-    vec1 = vec3;
+    if (signo == bignumber.signo) {
+        while (max1 > 0 || max2 > 0) {
+            int sum = z;
+            if (max1 > 0)
+                sum += vec1[max1 - 1];
+            if (max2 > 0)
+                sum += vec2[max2 - 1];
+            vec3[tam - 1] = sum % 10;
+            z = sum / 10;
+            max1--;
+            max2--;
+            tam--;
+        }
+        if (z == 1)
+            vec3[0] = 1;
+        else
+            vec3.erase(vec3.begin());
+
+        vec1 = vec3;
+        } 
+        else {
+        // Los números tienen signos diferentes, realizamos la resta
+        BigInteger temp = bignumber;
+        temp.signo = !temp.signo;
+        substract(temp);
+    }
 }
 
+/*
+Funcion: Product
+Entrada: Un objeto BigInteger
+Descripcion: Lo que hace esta funcion es multiplicar dos BigInteger, modificando el BigInteger que uso la funcion haciendo que quede con la multiplicacion.
+*/ 
 void BigInteger::product(BigInteger& bignumber) {
     vector<int> vec2 = bignumber.vec1;
     vector<int> vec3(vec1.size() + vec2.size(), 0);
-    int max1 = vec1.size();
-    int max2 = vec2.size();
-    int distancia = 0;
-    if (max1 >= max2) {
-        distancia = max1 - max2;
-        for (int i = 0; i < distancia; i++)
-            vec2.insert(vec2.begin(), 0);
-    } else {
-        distancia = max2 - max1;
-        for (int i = 0; i < distancia; i++)
-            vec1.insert(vec1.begin(), 0);
-    }
     for (int i = vec1.size() - 1; i >= 0; i--) {
-        int suma = 0;
+        int temp = 0;
         for (int j = vec2.size() - 1; j >= 0; j--) {
-            int actual = vec3[i + j + 1] + vec1[i] * vec2[j] + suma;
-            vec3[i + j + 1] = actual % 10;
-            suma = actual / 10;
+            int sum = vec3[i + j + 1] + vec1[i] * vec2[j] + temp;
+            vec3[i + j + 1] = sum % 10;
+            temp = sum / 10;
         }
-        vec3[i] += suma;
+        vec3[i] += temp;
     }
-    vec1 = vec3;
-}
 
-void BigInteger::substract(BigInteger& bignumber){
+    // Eliminar ceros principales
+    while (vec3.size() > 1 && vec3[0] == 0)
+        vec3.erase(vec3.begin());
+
+    vec1 = vec3;
+    if(signo==bignumber.signo)
+        signo =false;
+    else
+        signo=true;
+}
+/*
+Funcion: substract
+Entrada: Un objeto BigInteger
+Descripcion: Lo que hace es que al Biginteger que uso la funcion se le reste el Biginteger que recibe como entrada.
+*/ 
+void BigInteger::substract(BigInteger& bignumber) {
     int z = 0;
     vector<int> vec2 = bignumber.vec1;
-    vector<int> vec3;
     int max1 = vec1.size();
     int max2 = vec2.size();
-    int distancia = 0;
-    if (max1 >= max2) {
-        distancia = max1 - max2;
-        for (int i = 0; i < distancia; i++)
-            vec2.insert(vec2.begin(), 0);
-    } else {
-        distancia = max2 - max1;
-        for (int i = 0; i < distancia; i++)
-            vec1.insert(vec1.begin(), 0);
-    }
+    int tam = max(max1, max2);
+    vector<int> vec3(tam, 0);
 
-    for (int j = vec1.size() - 1; j >= 0; j--) {
+    if (signo == bignumber.signo) {
+        // Son del mismo signo 
+        for (int j = max1 - 1; j >= 0; j--) {
+            int res = vec1[j] - z;
 
-        if (vec1[j] < vec2[j] ) {
-            vec3.insert(vec3.begin(), ((vec1[j]-z+10) - vec2[j]) % 10);
-            z = 1;
-        } else {
-            vec3.insert(vec3.begin(), (vec1[j]-z) - vec2[j]);
-            z = 0;
+            if (max2 > 0)
+                res -= vec2[max2 - 1];
+
+            if (res < 0) {
+                vec3[j] = res + 10;
+                z = 1;
+            } else {
+                vec3[j] = res;
+                z = 0;
+            }
+
+            max2--;
         }
+
+        // Eliminar ceros principales
+        while (vec3.size() > 1 && vec3[0] == 0)
+            vec3.erase(vec3.begin());
+
+        vec1 = vec3;
+        signo = bignumber.signo;
+    } else {
+        // Los números tienen signos diferentes, realizamos la suma
+        BigInteger temp = bignumber;
+        temp.signo = !temp.signo;
+        add(temp);
     }
-    vec1 = vec3;
 }
-
+/*
+Funcion: quotient
+Entrada: Un objeto BigInteger
+Descripcion: Lo que hace es dividir el Biginteger que uso la funcion con otro Biginteger que recibe como entrada.
+*/ 
 void BigInteger::quotient(BigInteger& bignumber){
+    BigInteger dividendo(*this);
+    BigInteger divisor(bignumber);
+    string cero = "0";
+    string uno = "1";
+    BigInteger cociente(cero); 
+    BigInteger temp(uno);
+    bool encontrar = false;
 
+    if (dividendo < divisor) {
+        *this = BigInteger(cero);  
+        encontrar=true;
+    }
+    while (divisor <= dividendo && !encontrar) {
+        dividendo.substract(divisor);
+        cociente.add(temp);
+    }
+    vec1=cociente.vec1;
 }
-
-void BigInteger::pow(BigInteger& bignumber){
-
-}
-
+/*
+Funcion: remainder
+Entrada: Un objeto BigInteger
+Descripcion: Lo que hace es buscar el modulo de Dos BigInteger, el que uso la funcion con el que recibe de entrada. modificando el Biginteger que recibio la funcion quedando con el modulo.
+*/ 
 void BigInteger::remainder(BigInteger& bignumber){
+    BigInteger dividendo(*this);
+    BigInteger divisor(bignumber);
+    string cero = "0";
+    BigInteger cociente(cero); 
 
+    while (divisor <= dividendo) {
+        dividendo.substract(divisor);
+    }
+    vec1=dividendo.vec1;
 }
+/*
+Funcion: pow
+Entrada: entero negativo o positivo
+Descripcion: Lo que hace es que al Biginteger que usa esta funcion lo eleva al numero del exponente que recibe como entrada.
+*/ 
+void BigInteger::pow(int exponente) {
+    string uno ="1";
+    BigInteger result(uno);
+    BigInteger base(*this);
+    bool encontrar=false;
+    if(exponente<0){
+        exponente= -exponente;
+        if(exponente%2 ==0)
+            signo=false;
+        else
+            signo=true;
+    }
+    if (exponente==0){
+        encontrar=true;
+        vec1.clear();
+        vec1.push_back(1);
+    }
+    for (int i = 0; i < exponente && !encontrar; i++) {
+        result.product(base);
+    }
+    if(!encontrar)
+        vec1=result.vec1;
+}
+/*
+Funcion: toString
+Salida: Retorna un string 
+Descripcion:Lo que hace es que los digitos del vector que esta dentro de la clase BigInteger lo pasa a un string.
+*/ 
 string BigInteger::toString() {
     string cadena = "";
+
     for (int i = vec1.size()-1; i >= 0 ; i--) {
         cadena = char(vec1[i] + '0') + cadena;
     }
+    if (signo)
+        cadena= "-" + cadena;
     return cadena;
+} 
+/*
+Funcion: sumarListaValores
+Entrada: recibe un vector de BigInteger
+Salida:  Retorna un Biginteger 
+Descripcion: Lo que hace es que al BigInteger que use la funcion se le debe pasar una Lista de BigInteger y calcula la sumatoria de todos.
+*/ 
+BigInteger BigInteger::sumarListaValores(vector<BigInteger>& list){
+            string cero = "0";
+            BigInteger suma(cero);
+            for (vector<BigInteger>::iterator it = list.begin(); it != list.end(); ++it) {
+                suma.add(*it);
+            }
+            return suma;
 }
-void BigInteger::removerCeros(BigInteger& bignumber){
-    
-}
-
-
-void BigInteger::imprimir() {
-    if(signo==false){
-        cout << "-";
-    }
-    for (int i = 0; i < vec1.size(); i++)
-        cout << vec1[i];
-    cout << endl;
-}
+/*
+Funcion: multiplicarListaValores
+Entrada: recibe un vector de BigInteger
+Salida: Retorna un Big Integer
+Descripcion: Lo que hace es que al BigInteger que use la funcion se le debe pasar una Lista de BigInteger y calcula la multiplicacion de todos.
+*/ 
+BigInteger BigInteger::multiplicarListaValores(vector<BigInteger>& list){
+             string uno = "1";
+             BigInteger producto(uno);
+             for (vector<BigInteger>::iterator it = list.begin(); it != list.end(); ++it) {
+                    producto.product(*it);
+             }
+            return producto;
+ }
--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -1,22 +1,186 @@
-#include "bignumber.h"
+#include <string>
+#include <vector>
+#include <iostream>
 
+using namespace std;
+class BigInteger {
+    private:
+    vector<int> vec1;
+    bool signo;
+    public:
+    BigInteger(const string& cadena){
+        int start=0;
+        if(cadena[0] == '-') { 
+            start= 1;
+            signo = true;
+        } else {
+            signo = false;  
+        }
+        for (int i = start; i < cadena.size(); i++) {
+            vec1.push_back(cadena[i] - '0');
+        }
+    }
+    bool  BigInteger::operator==(BigInteger& bignumber){
+        bool ans=true;
+        int i=0;
+        if(vec1.size()!= bignumber.vec1.size())
+            ans=false;
+        while(ans && i < vec1.size()){
+            if(vec1[i]!=bignumber.vec1[i])
+                ans=false;
+            i++;
+         }
+        if(vec1.empty() && bignumber.vec1.empty())
+            ans=true;
+
+        return ans; 
+    }
+bool BigInteger::operator<(BigInteger& bignumber) {
+        bool result = false;
+        bool encontrar = false;
+        if (signo && !bignumber.signo) {
+            encontrar = true;
+            result = true;   
+        }
+        else if (!signo && bignumber.signo) 
+            encontrar = true;
+        else if (vec1.size() > bignumber.vec1.size()) { 
+            if (signo && bignumber.signo) { 
+                result = true;
+                encontrar = true;
+            }
+            else 
+                encontrar = true;
+        }
+        else if (vec1.size() < bignumber.vec1.size()) { 
+            if (signo && bignumber.signo) 
+                encontrar = true;
+            else {
+                result = true;
+                encontrar = true;
+            }
+        }
+        
+        else if(!encontrar) {
+                int i = 0;
+                while (i < vec1.size() && vec1[i] == bignumber.vec1[i]) {
+                    i++;
+                }
+                if (i < vec1.size() && vec1[i] < bignumber.vec1[i]) {
+                    if (signo && bignumber.signo) {
+                        result = false;
+                    }
+                    else {
+                        result = true;
+                    }
+                }
+                else{
+                    if (signo && bignumber.signo) {
+                        result = true;
+                    }
+                    else {
+                        result = false;
+                    }
+                }
+        }
+
+        return result;
+    }
+};
 int main() {
-    string num1 = "123456789";
-    BigInteger bignum1(num1);
-    bignum1.imprimir(); 
+    string num1;
+    string num2;
+    int casos=1;
+    while (cin >> num1 >> num2) {
+        string temporal1;
+        string temporal2;
+        string temporal3;
+        string temporal4;
+        bool encontrar = true;
+        int i = 0, tam1 = 0, tam2 = 0;
+        //Aqui dividí el numero 1, la parte entera lo coloque en el string temporal 1 y la parte decimal lo coloque en el string temporal 2
+        while (i < num1.size()) {
+            if (num1[i] != '.' && encontrar) {
+                temporal1 += num1[i];
+            }
+            if (num1[i] == '.') {
+                tam1 = i;
+                encontrar = false;
+            }
+            if (!encontrar) {
+                temporal2 += num1[i];
+            }
+            i++;
+        }
+        // aqui hice lo mismo de arriba para el segundo numero, solo que string temporal 3 y 4
+        i = 0;
+        encontrar = true;
+        while (i < num2.size()) {
+            if (num2[i] != '.' && encontrar) {
+                temporal3 += num2[i];
+            }
+            if (num2[i] == '.') {
+                tam2 = i;
+                encontrar = false;
+            }
+            if (!encontrar) {
+                temporal4 += num2[i];
+            }
+            i++;
+        }
+        //esto es para que de la respuesta rapidamente si la parte entera de un numero es superio a otra
+        if (tam1 > tam2) 
+            cout << "Case " << casos << ": Bigger" << endl;
+        else if (tam1 < tam2) 
+            cout << "Case " << casos << ": Smaller" << endl;
+        // si la parte entera tiene el mismo tamañao proceder a compararlos.
+        else {
+            // aqui lo que hago es quitar los 0 de la derecha tanto del primer numero como del segundo
+            int j = temporal2.size() - 1;
+            bool find = true;
+            while (j >= 0 && find) {
+                if (temporal2[j] == '0') {
+                    temporal2.pop_back();
+                } else {
+                    find = false;
+                }
+                j--;
+            }
 
-    BigInteger bignum2(bignum1);
-    bignum2.imprimir(); 
-    string num2 = bignum1.toString();
-    cout << num2 << endl;
-    string num3 = "15";
-    BigInteger prueba1(num3);
-    string num4 = "-04";
-    BigInteger prueba2(num4);
-    prueba2.imprimir();
-
-
-    
-
+            j = temporal4.size() - 1;
+            find = true;
+            while (j >= 0 && find) {
+                if (temporal4[j] == '0') {
+                    temporal4.pop_back();
+                } else {
+                    find = false;
+                }
+                j--;
+            }
+            BigInteger numero1(temporal1);
+            BigInteger numero2(temporal2);
+            BigInteger numero3(temporal3);
+            BigInteger numero4(temporal4);   
+           if (numero1 == numero3 && numero2 == numero4) {
+                cout << "Case " << casos << ": Same" << endl;
+            }
+            else if (numero1 == numero3) {
+                if (numero2 < numero4) {
+                    cout << "Case " << casos << ": Smaller" << endl;
+                }
+                else {
+                    cout << "Case " << casos << ": Bigger" << endl;
+                }
+            }
+            else if (numero1 < numero3) {
+                cout << "Case " << casos << ": Smaller" << endl;
+            }
+            else {
+                cout << "Case " << casos << ": Bigger" << endl;
+            }
+              
+        }
+        casos++;
+    }
     return 0;
 }
